f4e5fd1d98a5aa62c31fcb155e6d7dd245caa2fe
fadlyhts <fadlyhutasuhut1305@gmail.com>
2025-06-25 11:41:14 +0700

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 80,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: '',
         changeOrigin: true,
         secure: false,
       }

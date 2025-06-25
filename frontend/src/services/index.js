be660eff7aa3f30a92cd5d2457d6918122d68fbe
fadlyhts <fadlyhutasuhut1305@gmail.com>
2025-06-25 11:29:51 +0700

    Add index service

---
+++ frontend/src/services/index.js
@@ -0,0 +1,23 @@
+import api from './api.js';
+import authService from './authService.js';
+import adminService from './adminService.js';
+import driverService from './driverService.js';
+import vehicleService from './vehicleService.js';
+import deviceService from './deviceService.js';
+import sessionService from './sessionService.js';
+import passengerService from './passengerService.js';
+import reportService from './reportService.js';
+import reportsService from './reportsService.js';
+
+export {
+  api,
+  authService,
+  adminService,
+  driverService,
+  vehicleService,
+  deviceService,
+  sessionService,
+  passengerService,
+  reportService,
+  reportsService
+};
